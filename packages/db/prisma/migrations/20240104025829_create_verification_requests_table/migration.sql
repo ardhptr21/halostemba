6a25d8ef559a595afa9c2e77e5bb14bf70b8ba03
amda23 <bintangtriadmaja@gmail.com>
2024-01-04 10:02:47 +0700

    feat(db): create migration for verification user to student

---
+++ packages/db/prisma/migrations/20240104025829_create_verification_requests_table/migration.sql
@@ -0,0 +1,56 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `verified` on the `students` table. All the data in the column will be lost.
+  - Added the required column `major_id` to the `students` table without a default value. This is not possible if the table is not empty.
+  - Made the column `id_card` on table `students` required. This step will fail if there are existing NULL values in that column.
+  - Added the required column `banned_at` to the `users` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "Status" AS ENUM ('pending', 'approved', 'rejected');
+
+-- AlterTable
+ALTER TABLE "students" DROP COLUMN "verified",
+ADD COLUMN     "major_id" TEXT NOT NULL,
+ALTER COLUMN "id_card" SET NOT NULL;
+
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "banned_at" TIMESTAMP(3) NOT NULL;
+
+-- CreateTable
+CREATE TABLE "verify_requests" (
+    "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
+    "major_id" TEXT NOT NULL,
+    "id_card" TEXT NOT NULL,
+    "note" TEXT,
+    "status" "Status" NOT NULL DEFAULT 'pending',
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "verify_requests_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "majors" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "logo" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "majors_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verify_requests_user_id_key" ON "verify_requests"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "majors_name_key" ON "majors"("name");
+
+-- AddForeignKey
+ALTER TABLE "students" ADD CONSTRAINT "students_major_id_fkey" FOREIGN KEY ("major_id") REFERENCES "majors"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "verify_requests" ADD CONSTRAINT "verify_requests_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
