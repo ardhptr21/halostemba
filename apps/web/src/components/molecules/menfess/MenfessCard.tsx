ae09c66968d782541e6e994cd8e53e07856bb692
amda23 <bintangtriadmaja@gmail.com>
2024-01-16 22:49:07 +0700

    feat: delete menfess functionality

--- apps/web/src/components/molecules/menfess/MenfessCard.tsx
+++ apps/web/src/components/molecules/menfess/MenfessCard.tsx
@@ -3,13 +3,15 @@ import { MenfessEntity } from "@halostemba/entities";
 import {
   ChatBubbleIcon,
   DotsHorizontalIcon,
+  TrashIcon,
   TriangleDownIcon,
   TriangleUpIcon,
 } from "@radix-ui/react-icons";
-import { Box, Card, Flex, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Popover, Text } from "@radix-ui/themes";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
-import { ForwardedRef, forwardRef } from "react";
+import Link from "next/link";
+import { ForwardedRef, forwardRef, useState } from "react";
+import DeleteMenfessModal from "~/components/atoms/modals/DeleteMenfessModal";
 
 interface MenfessCardProps {
   redirect?: boolean;
@@ -20,18 +22,12 @@ function MenfessCard(
   { redirect, menfess }: MenfessCardProps,
   ref: ForwardedRef<HTMLDivElement>,
 ) {
-  const router = useRouter();
-
-  const handleRedirect = () => {
-    if (!redirect) return;
-
-    router.push(`/menfess/${menfess.id}`);
-  };
+  const Wrapper = redirect ? Link : "div";
 
   return (
     <Box ref={ref} width="100%">
-      <Card asChild className="w-full" onClick={handleRedirect}>
-        <article className={redirect ? "cursor-pointer" : ""}>
+      <Card asChild className="w-full">
+        <article>
           <Flex direction="row" gap="2">
             <Box>
               <Image
@@ -59,29 +55,35 @@ function MenfessCard(
                     2 menit yang lalu
                   </Text>
                 </Flex>
-                <DotsHorizontalIcon />
-              </Flex>
-              <Flex direction="column" gap="4">
-                <Text size="2" color="gray" className="whitespace-pre-line">
-                  {menfess.content}
-                </Text>
 
-                <Flex align="center" gap="3">
-                  <Flex align="center" asChild gap="2">
-                    <Text as="p" color="gray">
-                      <ChatBubbleIcon cursor="pointer" />
-                      <Text size="2">4 Replies</Text>
-                    </Text>
-                  </Flex>
-                  <Flex align="center" gap="1" asChild>
-                    <Text as="p" color="gray">
-                      <TriangleUpIcon className="text-slate-400" />
-                      <Text size="2">{menfess.score}</Text>
-                      <TriangleDownIcon />
-                    </Text>
+                <MenfessCardPopOver menfessId={menfess.id} />
+              </Flex>
+              <Wrapper
+                href={`/menfess/${menfess.id}`}
+                className={redirect ? "cursor-pointer" : ""}
+              >
+                <Flex direction="column" gap="4">
+                  <Text size="2" color="gray" className="whitespace-pre-line">
+                    {menfess.content}
+                  </Text>
+
+                  <Flex align="center" gap="3">
+                    <Flex align="center" asChild gap="2">
+                      <Text as="p" color="gray">
+                        <ChatBubbleIcon cursor="pointer" />
+                        <Text size="2">4 Replies</Text>
+                      </Text>
+                    </Flex>
+                    <Flex align="center" gap="1" asChild>
+                      <Text as="p" color="gray">
+                        <TriangleUpIcon className="text-slate-400" />
+                        <Text size="2">{menfess.score}</Text>
+                        <TriangleDownIcon />
+                      </Text>
+                    </Flex>
                   </Flex>
                 </Flex>
-              </Flex>
+              </Wrapper>
             </Flex>
           </Flex>
         </article>
@@ -89,5 +91,43 @@ function MenfessCard(
     </Box>
   );
 }
+
+interface MenfessCardPopOverProps {
+  menfessId: string;
+}
+
+const MenfessCardPopOver = ({ menfessId }: MenfessCardPopOverProps) => {
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
+
+  return (
+    <>
+      <DeleteMenfessModal
+        menfessId={menfessId}
+        open={openDeleteModal}
+        onOpenChange={setOpenDeleteModal}
+      />
+      <Popover.Root>
+        <Popover.Trigger>
+          <Button variant="ghost" style={{ cursor: "pointer" }}>
+            <DotsHorizontalIcon />
+          </Button>
+        </Popover.Trigger>
+        <Popover.Content align="end" size={"1"} style={{ width: 105 }}>
+          <Flex
+            align={"center"}
+            asChild
+            style={{ cursor: "pointer" }}
+            onClick={() => setOpenDeleteModal(true)}
+          >
+            <Text as="p" color="red">
+              <TrashIcon width={20} height={20} />
+              <Text>Delete</Text>
+            </Text>
+          </Flex>
+        </Popover.Content>
+      </Popover.Root>
+    </>
+  );
+};
 
 export default forwardRef(MenfessCard);
