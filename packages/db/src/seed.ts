8176b67ac616c1214618a706b193867afa940af5
Ardhi Putra <ardhiputrapradana21@gmail.com>
2024-03-01 19:28:50 +0700

    chore: production setup

--- packages/db/src/seed.ts
+++ packages/db/src/seed.ts
@@ -1,7 +1,8 @@
-import { PrismaClient } from "@prisma/client";
-import { MenfessSeeder } from "./seeders/menfess-seeder";
-import UserSeeder from "./seeders/user-seeder";
-import { MajorSeeder } from "./seeders/major-seeder";
+import { PrismaClient } from '@prisma/client';
+import { MenfessSeeder } from './seeders/menfess-seeder';
+import UserSeeder from './seeders/user-seeder';
+import { MajorSeeder } from './seeders/major-seeder';
+import ProductionSeeder from './seeders/production-seeder';
 
 async function main() {
   const table = process.argv[2];
@@ -9,23 +10,26 @@ async function main() {
   const client = new PrismaClient();
 
   switch (table) {
-    case "all":
+    case 'all':
       await UserSeeder(client);
       await MenfessSeeder(client, 20);
       await MajorSeeder(client);
       break;
-    case "user":
+    case 'user':
       await UserSeeder(client);
       break;
-    case "menfess":
+    case 'menfess':
       const count = process.argv[3] ? parseInt(process.argv[3]) : 20;
       await MenfessSeeder(client, count);
       break;
-    case "major":
+    case 'major':
       await MajorSeeder(client);
       break;
+    case 'production':
+      await ProductionSeeder(client);
+      break;
     default:
-      console.log("Invalid table name!");
+      console.log('Invalid table name!');
       break;
   }
 
