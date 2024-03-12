22c87fa1401c938d36304dbef74bda59c3f79568
Ardhi Putra <ardhiputrapradana21@gmail.com>
2024-03-12 15:26:28 +0700

    feat: update font

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,8 +1,17 @@
 import "@radix-ui/themes/styles.css";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 
 import { Theme } from "@radix-ui/themes";
 import GlobalProvider from "~/components/organisms/GlobalProvider";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+  style: ["italic", "normal"],
+  display: "swap",
+  variable: "--font-montserrat",
+});
 
 export default function RootLayout({
   children,
@@ -10,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="id" className="antialiased dark">
-      <body className="overflow-y-scroll">
+    <html lang="id" className={`antialiased dark ${montserrat.variable}`}>
+      <body className="overflow-y-scroll font-montserrat">
         <Theme appearance="dark">
           <GlobalProvider>{children}</GlobalProvider>
         </Theme>
